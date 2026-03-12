826beb9dce58c0bde78f7861281cdd7164a68323
IvanSP04 <ivan.sterlingp@uniagustiniana.edu.co>
2026-03-12 02:31:14 +0000

    Cambios 2.0

--- pagina-grupo/src/App.jsx
+++ pagina-grupo/src/App.jsx
@@ -1,9 +1,12 @@
+
 import Navbar from "./Componentes/Navbar";
+import MenuPrincipal from "./Componentes/MenuPrincipal";
 
 function App() {
   return (
     <>
       <Navbar />
+      <MenuPrincipal />
     </>
   );
 }
