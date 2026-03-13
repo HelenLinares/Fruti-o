4499291340530c7712e95282fc56b81a737d62c5
samuelRB17 <samuel.ramirezb@uniagustiniana.edu.co>
2026-03-13 05:06:50 +0000

    ComponenteAcercadeStarbucks/SamuelR

--- pagina-grupo/src/App.jsx
+++ pagina-grupo/src/App.jsx
@@ -1,12 +1,14 @@
 
 import Navbar from "./Componentes/Navbar";
 import MenuPrincipal from "./Componentes/MenuPrincipal";
+import AboutStarbucks from "./Componentes/AboutStarbucks";
 
 function App() {
   return (
     <>
       <Navbar />
       <MenuPrincipal />
+       <AboutStarbucks />
     </>
   );
 }
